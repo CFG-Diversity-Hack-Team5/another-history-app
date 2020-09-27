38d74a69eee6ad0ea194be71807313491e8e7757
ancalita <27920906+ancalita@users.noreply.github.com>
2020-09-27 08:11:49 +0100

    made changes to public blueprint

--- main/views/public.py
+++ main/views/public.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, redirect, render_template, url_for
 from main.models import Course, CourseLike, Module
-from sqlalchemy import func, and_
+from sqlalchemy import func
 from flask_login import current_user
 
 public_bp = Blueprint('public_bp', __name__)
 
 @public_bp.route('/', methods=['GET', 'POST'])
-def homepage():
+def index():
     popular_courses = Course.query.join(
         CourseLike, Course.id == CourseLike.course_id).group_by(Course.id).order_by(func.count().desc()).limit(3).all()
     popular_courses = [course.title for course in popular_courses]
@@ -18,7 +18,7 @@ def homepage():
     if current_user.is_authenticated:
         return redirect(url_for('user-dashboard'))
 
-    return render_template('placeholder.html',
+    return render_template('index.html',
                            popular_courses=popular_courses,
                            featured=featured_course.title,
                            related_courses=related_courses)
@@ -29,25 +29,15 @@ def browse_courses():
     return render_template('html', courses=courses)
 
 
-@public_bp.route('/course/<int:course_id>', methods=['GET','POST'])
+@public_bp.route('/courses/<int:course_id>', methods=['GET','POST'])
 def show_course(course_id):
     course = Course.query.filter_by(id=course_id).first()
-    week_one = Module.query.join(Course, Module.course_id == Course.id).filter(
-        and_(course_id == course_id, Module.order == 1)).first()
-    week_two = Module.query.join(Course, Module.course_id == Course.id).filter(
-        and_(course_id == course_id, Module.order == 2)).first()
-    week_three = Module.query.join(Course, Module.course_id == Course.id).filter(
-        and_(course_id == course_id, Module.order == 3)).first()
-    week_four = Module.query.join(Course, Module.course_id == Course.id).filter(
-        and_(course_id == course_id, Module.order == 4)).first()
+    modules = Module.query.join(Course, Module.course_id == Course.id).filter(course_id == course_id).all()
     books = course.books
     return render_template("placeholder.html", course=course,
-                           week_one=week_one,
-                           week_two=week_two,
-                           week_three=week_three,
-                           week_four=week_four,
+                           modules=modules,
                            books=books)
 
-@public_bp.route('/about', methods=['GET','POST'])
+@public_bp.route('/about', methods=['GET'])
 def about_us():
-    return render_template()
+    return render_template("about.html")
