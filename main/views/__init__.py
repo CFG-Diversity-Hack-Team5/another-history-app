3a3ef207579dabe8e3383645eff5164a40727baa
ancalita <27920906+ancalita@users.noreply.github.com>
2020-09-29 07:47:18 +0100

    start connecting index template

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -19,7 +19,7 @@ bootstrap = Bootstrap()
 
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates')
+    app = Flask(__name__, static_folder='../static', template_folder='../templates')
     app.config.from_object(os.environ['APP_SETTINGS'])
 
     csrf.init_app(app)
--- main/views/public.py
+++ main/views/public.py
@@ -8,13 +8,11 @@ public_bp = Blueprint('public_bp', __name__)
 
 @public_bp.route('/', methods=['GET', 'POST'])
 def index():
-    popular_courses = Course.query.join(
+    '''popular_courses = Course.query.join(
         CourseLike, Course.id == CourseLike.course_id).group_by(Course.id).order_by(func.count().desc()).limit(3).all()
-    popular_courses = [course.title for course in popular_courses]
-    featured_course = Course.query.filter(Course.body.like('%Featured_Person_Name%')).first()  # to do
-    featured_category = featured_course.category
-    related_courses = Course.query.filter_by(category=featured_category).limit(3).all()
-    related_courses = [course.title for course in related_courses]
+    popular_courses = [course.title for course in popular_courses]'''
+
+    courses = Course.query.all()
 
     if current_user.is_authenticated:
         if current_user.access == 1:
@@ -22,16 +20,13 @@ def index():
         else:
             return redirect(url_for('admin_bp.show_admin_dashboard'))
 
-    return render_template('index.html',
-                           popular_courses=popular_courses,
-                           featured=featured_course.title,
-                           related_courses=related_courses)
+    return render_template('index.html', courses=courses)
 
 
-@public_bp.route('/courses', methods=['GET', 'POST'])
+'''@public_bp.route('/courses', methods=['GET', 'POST'])
 def browse_courses():
     courses = Course.query.all()
-    return render_template('html', courses=courses)
+    return render_template('html', courses=courses)'''
 
 
 @public_bp.route('/courses/<int:cid>', methods=['GET', 'POST'])
